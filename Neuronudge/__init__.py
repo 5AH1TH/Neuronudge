64119cb7a61f4153d0b5e60c80a52a90bcdd4957
5AH1TH <sahithch@outlook.com>
2025-09-01 18:41:19 -0700

    didn't change much

--- Neuronudge/__init__.py
+++ Neuronudge/__init__.py
@@ -50,4 +50,12 @@ def create_app():
     with app.app_context():
         db.create_all()
 
+
+    @app.after_request
+    def add_header(response):
+        response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "0"
+        return response
+    
     return app
